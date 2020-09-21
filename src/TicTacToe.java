58d9e5d1182a2a86683b9679cccd37a2732f99de
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-21 16:04:41 +0100

    Moved printGameBoard into a separate function

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,13 +7,15 @@ public class TicTacToe {
                 {' ', '|', ' ', '|', ' '},
                 {' ', '|', ' ', '|', ' '}
         };
+    printGameBoard(gameBoard);
+    }
 
+    public static void printGameBoard(char[][] gameBoard) {
         for(char[] row : gameBoard) {
             for(char c : row) {
                 System.out.print(c);
             }
             System.out.println();
         }
-
     }
 }
