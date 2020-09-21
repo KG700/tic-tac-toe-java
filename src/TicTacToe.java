026973beeb6ebd13cbafdb217320f7fd5fdb7322
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-21 16:20:45 +0100

    Created method to place piece at given position

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -16,6 +16,10 @@ public class TicTacToe {
     int pos = scan.nextInt();
 
     System.out.println(pos);
+
+    placePiece(gameBoard, pos, "Player");
+    printGameBoard(gameBoard);
+
     }
 
     public static void printGameBoard(char[][] gameBoard) {
@@ -24,6 +28,47 @@ public class TicTacToe {
                 System.out.print(c);
             }
             System.out.println();
+        }
+    }
+
+    public static void placePiece(char[][] gameBoard, int pos, String user) {
+
+        char symbol = 'X';
+
+        if(user.equals("player")) {
+            symbol = 'X';
+        } else if (user.equals("cpu")) {
+            symbol = 'O';
+        }
+
+        switch(pos) {
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
         }
     }
 }
