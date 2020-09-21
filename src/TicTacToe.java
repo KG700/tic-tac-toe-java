0dd07da9b8181260d0751ba034ec369aebc1ffdc
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-21 16:05:50 +0100

    Updated formatting of game board

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,9 +2,9 @@ public class TicTacToe {
     public static void main(String[] args) {
         char[][] gameBoard = {
                 {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '},
-                {' ', '|', ' ', '|', ' '},
-                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '}
         };
     printGameBoard(gameBoard);
